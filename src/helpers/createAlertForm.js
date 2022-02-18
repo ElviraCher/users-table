7b85831c44906116454cec1603770b5c5126f366
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-18 18:03:04 +0300

    Add block for alert when form edit

---
+++ src/helpers/createAlertForm.js
@@ -0,0 +1,16 @@
+export default function createAlertForm() {
+  const a = document.querySelector(".alert__container");
+
+  if (!a) {
+    const root = document.querySelector("#root");
+    const alertContainer = document.createElement("div");
+    const alertMassage = document.createElement("p");
+    alertContainer.classList.add("alert__container");
+    alertMassage.classList.add("alert__massage");
+    alertContainer.appendChild(alertMassage);
+    root.appendChild(alertContainer);
+    alertMassage.textContent = `Вы находитесь в форме редактирования. 
+  Нажмите кнопку 'remove', если хотите отменить изменения и кнопку 'submit', 
+  если хотите сохранить изменения`;
+  }
+}
