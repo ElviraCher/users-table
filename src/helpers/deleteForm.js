05e608407afa987103fb93cb90fe5e631287a913
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-18 18:06:34 +0300

    Add possibility to remove alert form

--- src/helpers/deleteForm.js
+++ src/helpers/deleteForm.js
@@ -1,7 +1,11 @@
-export default function deleteForm(e) {
-  e.preventDefault();
+export default function deleteForm() {
   const root = document.querySelector("#root");
   const form = document.querySelector(".form");
   form.reset();
   root.removeChild(form);
+
+  const alertForm = document.querySelector(".alert__container");
+  if (alertForm) {
+    root.removeChild(alertForm);
+  }
 }
