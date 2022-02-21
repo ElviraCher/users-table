bbbc908e5b95a91fb6399d944a5d58abde5510f2
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-21 11:54:59 +0300

    Add classes to active buttons

--- src/helpers/createPage.js
+++ src/helpers/createPage.js
@@ -5,16 +5,24 @@ export default async function createPage(data) {
   const rowsCount = data;
   const rowsOnPage = 10;
   const buttons = document.querySelectorAll(".button__counter");
+  buttons[0].classList.add("button__counter--active");
 
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
       e.preventDefault();
+
+      const previousActiveButton = document.querySelector(
+        ".button__counter--active"
+      );
+      previousActiveButton.classList.remove("button__counter--active");
+
       const pageNumber = button.innerHTML;
       const start = (pageNumber - 1) * rowsOnPage;
       const end = start + rowsOnPage;
       const currentData = rowsCount.slice(start, end);
       removeTableBody();
       createTableBody(rowsOnPage, 4, currentData);
+      button.classList.add("button__counter--active");
     });
   });
 }
