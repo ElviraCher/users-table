4e7fdf7dc13991785bde5198fb675769abd749a3
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-22 10:41:48 +0300

    Add comments

--- src/helpers/editTableForm.js
+++ src/helpers/editTableForm.js
@@ -2,8 +2,10 @@ import deleteForm from "./deleteForm";
 
 export default function editTableForm(tr) {
   const row = tr;
+  // присваиваем класс строке, чтобы было понятно, что редактируем именно её
   row.classList.add("row__edited");
 
+  // переносим данные из редактируемой строки в инпуты и создаём кнопки формы
   const nameInput = document.querySelector("#name__input");
   const secondNameInput = document.querySelector("#second-name__input");
   const aboutInput = document.querySelector("#about__input");
@@ -16,14 +18,17 @@ export default function editTableForm(tr) {
   secondNameInput.value = row.querySelectorAll(".cell")[1].innerText;
   aboutInput.value = row.querySelectorAll(".cell")[2].innerText;
 
+  // создаём функцию для удаления класса строки, чтобы вызвать её,
+  // когда строка уже не является редактируемой
   const removeClass = () => {
     row.classList.remove("row__edited");
   };
 
+  // функция, которая будет вызвана при нажатии на кнопку "Submit"
   function submitFunction(e) {
     // предотвратим перезагрузку страницы
     e.preventDefault();
-
+    // писваиваем ячейкам в таблице значения из инпутов
     row.querySelectorAll(".cell")[0].innerText = nameInput.value;
     row.querySelectorAll(".cell")[1].innerText = secondNameInput.value;
     row.querySelectorAll(
@@ -32,11 +37,12 @@ export default function editTableForm(tr) {
     row.querySelectorAll(
       ".cell"
     )[3].innerHTML = `<div class = "cell__eye cell__eye--${eyeColorSelect.value}"></div>`;
-
+    // удаляем форму и класс редактируемой ячейки
     deleteForm();
     removeClass();
   }
 
+  // навешиваем обработчики событий на кнопки
   buttonOk.addEventListener("click", submitFunction);
   buttonCancel.addEventListener("click", deleteForm);
   buttonCancel.addEventListener("click", removeClass);
