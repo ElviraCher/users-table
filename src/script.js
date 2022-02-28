8e3708aa58d1e4fc98d36fde07acfaed0d646953
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-02-28 21:10:46 +0300

    Edit sortFunction.js

--- src/script.js
+++ src/script.js
@@ -1,29 +1,19 @@
 import "./style.scss";
 import { USER_DATA } from "./constants";
 import drawPage from "./drawPage";
-import sortFunction from "./helpers/sortFunction";
 import createTableBody from "./helpers/createTableBody";
 import createPage from "./helpers/createPage";
 import createPageButtons from "./helpers/createPageButtons";
+import hideAndSortColumn from "./helpers/hideAndSortColumn";
 
 (async function main() {
   const arr = USER_DATA;
   await drawPage();
+  hideAndSortColumn();
 
   // вызываем функцию, которая будет в цикле наполнять тело таблицы
   await createTableBody(10, 4, arr);
   await createPageButtons(arr);
 
-  const table = document.querySelector(".table__body");
-
   await createPage(arr);
-
-  // вешаем обработчик событий на все элементы с кассом "table__heading"
-  // по клику на любую из ячеек с заголовками в шапке таблице
-  // будет вызвана функция сортировки
-  document
-    .querySelectorAll(".table__heading")
-    .forEach((th, index) =>
-      th.addEventListener("click", () => sortFunction(index, table))
-    );
 })();
