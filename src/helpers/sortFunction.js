4183e16f684c931b64c3b37931c08dcb60ba2369
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-01 12:11:16 +0300

    Remove redundant "slice"

--- src/helpers/sortFunction.js
+++ src/helpers/sortFunction.js
@@ -3,16 +3,13 @@
 // 2). таблица
 
 // создаём массив из строк таблицы
-// отрезаем первую строку, так как там содержатся наши заголовки
 // сортруем по тексту (в алфавитном порядке), содержащемуся в ячейке
 // далее вставляем отсортированные ячейки в таблицу
 
 export default function sortFunction(index, table) {
-  const sortedRows = Array.from(table.rows)
-    .slice(1)
-    .sort((cellA, cellB) =>
-      cellA.cells[index].innerHTML > cellB.cells[index].innerHTML ? 1 : -1
-    );
+  const sortedRows = Array.from(table.rows).sort((cellA, cellB) =>
+    cellA.cells[index].innerHTML > cellB.cells[index].innerHTML ? 1 : -1
+  );
 
   table.append(...sortedRows);
 }
