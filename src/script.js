d7350ee1480d63e9c1eec51ebca8c16ec3ebccc8
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-01 12:13:46 +0300

    Add listener for ".inner-div__sorted"

--- src/script.js
+++ src/script.js
@@ -1,19 +1,31 @@
 import "./style.scss";
 import { USER_DATA } from "./constants";
 import drawPage from "./drawPage";
+import sortFunction from "./helpers/sortFunction";
 import createTableBody from "./helpers/createTableBody";
 import createPage from "./helpers/createPage";
 import createPageButtons from "./helpers/createPageButtons";
-import hideAndSortColumn from "./helpers/hideAndSortColumn";
+import hideColumn from "./helpers/hideColumn";
 
 (async function main() {
   const arr = USER_DATA;
   await drawPage();
-  hideAndSortColumn();
+  hideColumn();
 
   // вызываем функцию, которая будет в цикле наполнять тело таблицы
   await createTableBody(10, 4, arr);
   await createPageButtons(arr);
 
+  const table = document.querySelector(".table__body");
+
   await createPage(arr);
+
+  // вешаем обработчик событий на все элементы с кассом ".inner-div__sort"
+  // по клику на внутренний див сортировки в шапке таблицы
+  // будет вызвана функция сортировки
+  document
+    .querySelectorAll(".inner-div__sort")
+    .forEach((sortDiv, index) =>
+      sortDiv.addEventListener("click", () => sortFunction(index, table))
+    );
 })();
